b8acc338555dba4036d2ec83b6af06592faf0361
Jason Rushton <github.com@schtuph.com>
2014-10-30 11:38:46 -0600

    ws test

---
+++ server.js
@@ -0,0 +1,23 @@
+var WebSocketServer = require('ws').Server,
+	http = require('http'),
+	express = require('express'),
+	app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+server.listen(3000);
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+	var id = setInterval(function() {
+		ws.send(JSON.stringify(process.memoryUsage()), function() { /* ignore errors */ });
+	}, 1000);
+
+	console.log('started client interval');
+
+	ws.on('close', function() {
+		console.log('stopping client interval');
+		clearInterval(id);
+	});
+});
